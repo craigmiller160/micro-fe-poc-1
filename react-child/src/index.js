c7d520d9cc7b1e8f8c2d0ed0e34cb4985a7f8e5d
Craig Miller <craigmiller160@gmail.com>
2021-08-07 08:50:40 -0400

    Setup web component registering

--- react-child/src/index.js
+++ react-child/src/index.js
@@ -1,5 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from "./App";
+import {wrapAndRegisterWebComp} from "./createWebComp";
 
 ReactDOM.render(<App />, document.querySelector('#root'));
+
+wrapAndRegisterWebComp('react-child', App);
