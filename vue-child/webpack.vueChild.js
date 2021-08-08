9af9062c9c9526baab55707ec693775f01577d48
Craig Miller <craigmiller160@gmail.com>
2021-08-08 16:33:02 -0400

    Working on webpack config

---
+++ vue-child/webpack.vueChild.js
@@ -0,0 +1,45 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const ModuleFederationPlugin = require("webpack/lib/container/ModuleFederationPlugin");
+const TerserPlugin = require('terser-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+
+module.exports = {
+    mode: process.env.NODE_ENV,
+    entry: path.join(__dirname, 'src', 'VueChildIndex.js'),
+    output: {
+        path: path.join(__dirname, 'build'),
+        filename: 'assets/js/[name].[contenthash].js'
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: path.join(__dirname, 'src', 'VueChildIndex.html')
+        }),
+        new TerserPlugin(),
+        new CleanWebpackPlugin()
+    ],
+    devServer: {
+        port: 3003,
+        contentBase: path.join(__dirname, 'src'),
+        hot: true
+    },
+    module: {
+        rules: [
+            {
+                test: /\.vue$/,
+                use: 'vue-loader'
+            },
+            {
+                test: /\.(css|scss)$/,
+                use: [
+                    "style-loader",
+                    'css-loader'
+                ],
+            },
+            {
+                test: /\.(jpg|jpeg|png|gif|mp3|svg)$/,
+                use: ["file-loader"]
+            },
+        ]
+    }
+}
