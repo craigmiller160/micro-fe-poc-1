b81e02e911f1ae55198ba4c0ebfd4b0eed723764
Craig Miller <craigmiller160@gmail.com>
2021-08-08 14:36:25 -0400

    Build full reactChildServer.

--- react-child/server/reactChildServer.js
+++ react-child/server/reactChildServer.js
@@ -3,11 +3,32 @@ const fs = require('fs');
 const path = require('path');
 
 const app = express();
+const buildRoot = path.resolve('..', 'build');
+
+const getContentType = (ext) => {
+    switch (ext) {
+        case '.html': return 'text/html';
+        case '.css': return 'text/css';
+        case '.js': return 'application/javascript';
+        default:
+            throw new Error(`Cannot find content-type for extension: ${ext}`);
+    }
+}
 
 app.get('/**', (req, res) => {
     const { path } = req;
+    const filePath = path.resolve(buildRoot, path);
+    if (!fs.existsSync(filePath)) {
+        res.status(404);
+        res.send(`Cannot find file: ${filePath}`);
+        return;
+    }
+
+    const extension = path.extname(filePath);
+
     res.set({
-        'Content-Type': 'text/html'
+        'Content-Type': getContentType(extension),
+        'Cache-Control': 'public, max-age=604800'
     })
     res.send('Hello World');
 });
