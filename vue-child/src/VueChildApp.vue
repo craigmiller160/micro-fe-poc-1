f4c0337910b132d8459bc9abb8ac8986479ffa1b
Craig Miller <craigmiller160@gmail.com>
2021-08-08 16:27:48 -0400

    Building very simple vue app

---
+++ vue-child/src/VueChildApp.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="VueChild">
+    <h1>Vue Child</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "VueChildApp"
+}
+</script>
+
+<style scoped>
+  .VueChild {
+    background-color: orange;
+  }
+</style>
