c4375738b66b4f36c3d640c0209f6de33bb8a7a4
Craig Miller <craigmiller160@gmail.com>
2021-08-08 16:01:03 -0400

    Added svelte project

---
+++ svelte-child/src/SvelteApp.svelte
@@ -0,0 +1,9 @@
+<style>
+    .SvelteChild {
+        background-color: yellow;
+    }
+</style>
+
+<div class="SvelteChild">
+    <h1>Svelte Child</h1>
+</div>
---
+++ svelte-child/src/SvelteIndex.js
@@ -0,0 +1,7 @@
+import SvelteApp from './SvelteApp.svelte';
+
+const app = new SvelteApp({
+    target: document.body
+});
+
+export default app;
