1bfc2485d292df343e999b4a77729c00809d92e2
Craig Miller <craigmiller160@gmail.com>
2021-08-08 15:31:26 -0400

    Got react child server working

--- server/reactChildServer.js
+++ server/reactChildServer.js
@@ -1,45 +1,6 @@
-const express = require('express');
-const fs = require('fs');
+const startServer = require('./server');
 const path = require('path');
 
-const app = express();
-const buildRoot = path.resolve(__dirname, '..', 'build');
+const buildRoot = path.resolve(__dirname, '..', 'react-child', 'build');
 
-const getContentType = (ext) => {
-    switch (ext) {
-        case '.html': return 'text/html';
-        case '.css': return 'text/css';
-        case '.js': return 'application/javascript';
-        default:
-            throw new Error(`Cannot find content-type for extension: ${ext}`);
-    }
-}
-
-app.get('/**', (req, res) => {
-    console.log(`Received Request: ${req.path}`);
-    const filePath = path.join(buildRoot, req.path);
-    if (!fs.existsSync(filePath)) {
-        console.error(`Cannot find file: ${filePath}`);
-        res.status(404);
-        res.set({
-            'Content-Type': 'text/plain'
-        })
-        res.send(`Cannot find file: ${filePath}`);
-        return;
-    }
-
-    console.log(`Returning file: ${filePath}`);
-
-    const extension = path.extname(filePath);
-    const content = fs.readFileSync(filePath, 'utf8');
-
-    res.set({
-        'Content-Type': getContentType(extension),
-        'Cache-Control': 'public, max-age=604800'
-    })
-    res.send(content);
-});
-
-app.listen(3001, () => {
-    console.log('Listening on port 3001');
-});
+startServer(buildRoot, 3001);
---
+++ server/server.js
@@ -0,0 +1,48 @@
+const express = require('express');
+const fs = require('fs');
+const path = require('path');
+
+const getContentType = (ext) => {
+    switch (ext) {
+        case '.html': return 'text/html';
+        case '.css': return 'text/css';
+        case '.js': return 'application/javascript';
+        default:
+            throw new Error(`Cannot find content-type for extension: ${ext}`);
+    }
+};
+
+const startServer = (buildRoot, port) => {
+    const app = express();
+
+    app.get('/**', (req, res) => {
+        console.log(`Received Request: ${req.path}`);
+        const filePath = path.join(buildRoot, req.path);
+        if (!fs.existsSync(filePath)) {
+            console.error(`Cannot find file: ${filePath}`);
+            res.status(404);
+            res.set({
+                'Content-Type': 'text/plain'
+            })
+            res.send(`Cannot find file: ${filePath}`);
+            return;
+        }
+
+        console.log(`Returning file: ${filePath}`);
+
+        const extension = path.extname(filePath);
+        const content = fs.readFileSync(filePath, 'utf8');
+
+        res.set({
+            'Content-Type': getContentType(extension),
+            'Cache-Control': 'public, max-age=604800'
+        })
+        res.send(content);
+    });
+
+    app.listen(port, () => {
+        console.log(`Listening on port: ${port}`);
+    });
+};
+
+module.exports = startServer;
