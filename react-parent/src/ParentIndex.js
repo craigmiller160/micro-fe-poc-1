8c1398325e1fc20144410ee8dfbe0ab874915ab5
Craig Miller <craigmiller160@gmail.com>
2021-08-08 13:37:51 -0400

    Got module federated web component working.

--- react-parent/src/ParentIndex.js
+++ react-parent/src/ParentIndex.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import ParentApp from "./ParentApp";
-import 'reactChild/ReactChildWC';
 
-ReactDOM.render(<ParentApp />, document.querySelector('#root'));
+import('reactChild/ReactChildWC')
+    .then(() => {
+        ReactDOM.render(<ParentApp />, document.querySelector('#root'));
+    })
+    .catch(ex => console.log('WebCompError', ex))
