c7710cfe350125cc0f308274b0412e94d93b5975
Craig Miller <craigmiller160@gmail.com>
2021-08-08 21:53:46 -0400

    Finished react pagination

--- react-child/src/components/Pager.js
+++ react-child/src/components/Pager.js
@@ -1,21 +1,39 @@
 import React from 'react';
 import classes from './Pager.css';
-import {Route, Switch, useLocation, useRouteMatch} from 'react-router';
+import {Route, Switch, useHistory, useLocation, useRouteMatch} from 'react-router';
 import Page from './Page';
 
 const Pager = () => {
     const location = useLocation();
+    const history = useHistory();
 
     const btnClasses1 = location.pathname === '/page/1' ? classes.active : '';
     const btnClasses2 = location.pathname === '/page/2' ? classes.active : '';
     const btnClasses3 = location.pathname === '/page/3' ? classes.active : '';
 
+    const goToPage = (pageNum) => history.push(`/page/${pageNum}`);
+
     return (
         <div className={ classes.Pager }>
             <div className={ classes.buttons }>
-                <button className={ btnClasses1 }>Page 1</button>
-                <button className={ btnClasses2 }>Page 2</button>
-                <button className={ btnClasses3 }>Page 3</button>
+                <button
+                    className={ btnClasses1 }
+                    onClick={ () => goToPage(1) }
+                >
+                    Page 1
+                </button>
+                <button
+                    className={ btnClasses2 }
+                    onClick={ () => goToPage(2) }
+                >
+                    Page 2
+                </button>
+                <button
+                    className={ btnClasses3 }
+                    onClick={ () => goToPage(3) }
+                >
+                    Page 3
+                </button>
             </div>
             <div>
                 <Switch>
