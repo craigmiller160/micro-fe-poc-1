1ba2899ed2bc706a3b72eb9a7b2775de697a2447
Craig Miller <craigmiller160@gmail.com>
2021-08-09 14:40:35 -0400

    Got svelte router fully implemented.

--- svelte-child/src/components/Pager.svelte
+++ svelte-child/src/components/Pager.svelte
@@ -1,6 +1,26 @@
 <script>
     import { Route, Link } from 'svelte-routing';
     import Page from './Page.svelte';
+    import { onMount, onDestroy } from 'svelte';
+    import { globalHistory } from 'svelte-routing/src/history';
+
+    let pathname = window.location.pathname;
+    let unsubscribe;
+
+    onMount(() => {
+        unsubscribe = globalHistory.listen((arg) => {
+            console.log('SvelteListen', arg);
+            pathname = arg.location.pathname;
+        });
+    });
+
+    onDestroy(() => {
+        unsubscribe();
+    });
+
+    $: btnActive1 = pathname === '/page/1';
+    $: btnActive2 = pathname === '/page/2';
+    $: btnActive3 = pathname === '/page/3';
 </script>
 
 <style>
@@ -29,7 +49,13 @@
 <div class="Pager">
     <div class="Buttons">
         <Link to="/page/1">
-            <button>Page 1</button>
+            <button class:active={ btnActive1 }>Page 1</button>
+        </Link>
+        <Link to="/page/2">
+            <button class:active={ btnActive2 }>Page 2</button>
+        </Link>
+        <Link to="/page/3">
+            <button class:active={ btnActive3 }>Page 3</button>
         </Link>
     </div>
     <div>
