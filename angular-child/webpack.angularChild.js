371805eb990f485fe1689febbfb83ccbd4178be2
Craig Miller <craigmiller160@gmail.com>
2021-08-09 16:04:19 -0400

    Lots more angular config

--- angular-child/webpack.angularChild.js
+++ angular-child/webpack.angularChild.js
@@ -3,6 +3,27 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const TerserPlugin = require('terser-webpack-plugin');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const ngw                     = require('@ngtools/webpack');
+
+const devTsLoader = {
+    test: /\.ts$/,
+    loaders: [
+        {
+            loader: 'awesome-typescript-loader',
+            options: {
+                configFileName: helpers.root('tsconfig.json')
+            }
+        },
+        'angular2-template-loader',
+        'angular-router-loader'
+    ],
+    exclude: [/node_modules/]
+};
+
+const prodTsLoader = {
+    test: /(?:\.ngfactory\.js|\.ngstyle\.js|\.ts)$/,
+    loader: '@ngtools/webpack'
+};
 
 module.exports = {
     mode: process.env.NODE_ENV,
@@ -22,7 +43,10 @@ module.exports = {
         new HtmlWebpackPlugin({
             template: path.join(__dirname, 'src', 'AngularChildIndex.html')
         }),
-        new TerserPlugin(),
+        process.env.NODE_ENV === 'production' ? new ngw.AngularCompilerPlugin({
+            tsConfigPath: path.join(__dirname, 'tsconfig.json'),
+            entryModule: path.join('src', 'app', 'app.module#AppModule')
+        }) : new TerserPlugin(),
         new CleanWebpackPlugin(),
         new MiniCssExtractPlugin({
             filename: 'assets/css/[name].[contenthash].css'
@@ -35,6 +59,7 @@ module.exports = {
     },
     module: {
         rules: [
+            process.env.NODE_ENV === 'production' ? prodTsLoader : devTsLoader,
             {
                 test: /\.html$/,
                 loader: 'html-loader'
