a265cc6565221d4eb8d6cd18cf2297a0af72a40b
Craig Miller <craigmiller160@gmail.com>
2021-08-09 14:27:16 -0400

    Working on svelte routing and refactored react routing.

--- react-child/src/components/Pager.js
+++ react-child/src/components/Pager.js
@@ -2,6 +2,7 @@ import React from 'react';
 import classes from './Pager.css';
 import {Route, Switch, useHistory, useLocation, useRouteMatch} from 'react-router';
 import Page from './Page';
+import {Link, NavLink} from 'react-router-dom';
 
 const Pager = () => {
     const location = useLocation();
@@ -11,29 +12,30 @@ const Pager = () => {
     const btnClasses2 = location.pathname === '/page/2' ? classes.active : '';
     const btnClasses3 = location.pathname === '/page/3' ? classes.active : '';
 
-    const goToPage = (pageNum) => history.push(`/page/${pageNum}`);
-
     return (
         <div className={ classes.Pager }>
             <div className={ classes.buttons }>
-                <button
-                    className={ btnClasses1 }
-                    onClick={ () => goToPage(1) }
-                >
-                    Page 1
-                </button>
-                <button
-                    className={ btnClasses2 }
-                    onClick={ () => goToPage(2) }
-                >
-                    Page 2
-                </button>
-                <button
-                    className={ btnClasses3 }
-                    onClick={ () => goToPage(3) }
-                >
-                    Page 3
-                </button>
+                <Link to="/page/1">
+                    <button
+                        className={ btnClasses1 }
+                    >
+                        Page 1
+                    </button>
+                </Link>
+                <Link to="/page/2">
+                    <button
+                        className={ btnClasses2 }
+                    >
+                        Page 2
+                    </button>
+                </Link>
+                <Link to="/page/3">
+                    <button
+                        className={ btnClasses3 }
+                    >
+                        Page 3
+                    </button>
+                </Link>
             </div>
             <div>
                 <Switch>
