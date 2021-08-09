6264a49054dab49a5eb7efa7ed6220e61f72f681
Craig Miller <craigmiller160@gmail.com>
2021-08-09 15:22:58 -0400

    Got React to update routes in response to global router.

--- react-child/src/useGlobalRouter.js
+++ react-child/src/useGlobalRouter.js
@@ -4,17 +4,23 @@ import {useEffect} from 'react';
 export default () => {
     const history = useHistory();
     useEffect(() => {
+        let dispatching = false;
         const historyUnsubscribe = history.listen((location) => {
             const event = new CustomEvent('microFrontendGlobalRouter', {
                 detail: {
                     pathname: location.pathname
                 }
             });
+            dispatching = true;
             window.dispatchEvent(event);
         });
 
         const globalRouterListener = (event) => {
-            console.log('ReactReceivedEvent', event);
+            if (!dispatching) {
+                history.push(event.detail.pathname);
+            } else {
+                dispatching = false;
+            }
         };
 
         window.addEventListener('microFrontendGlobalRouter', globalRouterListener, true);
