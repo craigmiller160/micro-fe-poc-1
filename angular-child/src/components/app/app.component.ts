57a257024566840c7366b03bfd110c067a93d0ef
Craig <craigmiller160@gmail.com>
2021-08-10 22:02:48 -0400

    Split html into separate file

--- angular-child/src/components/app/app.component.ts
+++ angular-child/src/components/app/app.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 
 @Component({
-    selector: 'app-component',
-    template: `<div>Angular Child</div>`
+    templateUrl: './app.component.html',
+    selector: 'app-component'
 })
 export class AppComponent {}
