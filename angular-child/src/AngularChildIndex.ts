fd6f9fae13600fcdf2e58e7627b4a553ad5886bc
Craig <craigmiller160@gmail.com>
2021-08-10 22:02:06 -0400

    More refactoring

--- angular-child/src/AngularChildIndex.ts
+++ angular-child/src/AngularChildIndex.ts
@@ -1,14 +1,5 @@
 import 'zone.js';
-import { NgModule, Component } from '@angular/core';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
-import { BrowserModule } from '@angular/platform-browser';
-import { AppComponent } from './components/app/app.component';
-
-@NgModule({
-    declarations: [AppComponent],
-    imports: [BrowserModule],
-    bootstrap: [AppComponent]
-})
-export class AppModule {}
+import { AppModule } from './app.module';
 
 platformBrowserDynamic().bootstrapModule(AppModule);
