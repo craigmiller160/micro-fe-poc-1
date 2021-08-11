0190a61e960113b9f928775c4a9b07cb16d18be6
Craig <craigmiller160@gmail.com>
2021-08-10 21:55:16 -0400

    Got the app to start

--- angular-child/webpack.angularChild.js
+++ angular-child/webpack.angularChild.js
@@ -2,7 +2,7 @@ const path = require('path');
 
 module.exports = {
     mode: process.env.NODE_ENV,
-    entry: path.join(__dirname, 'src', 'VueChildIndex.js'),
+    entry: path.join(__dirname, 'src', 'AngularChildIndex.ts'),
     output: {
         path: path.join(__dirname, 'build'),
         filename: 'assets/js/[name].[contenthash].js'
