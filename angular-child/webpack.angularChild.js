df8faa52a4180d961295a5f0534937122565156f
Craig <craigmiller160@gmail.com>
2021-08-10 21:52:05 -0400

    More webpack tweaks

--- angular-child/webpack.angularChild.js
+++ angular-child/webpack.angularChild.js
@@ -1,5 +1,12 @@
+const path = require('path');
 
 module.exports = {
+    mode: process.env.NODE_ENV,
+    entry: path.join(__dirname, 'src', 'VueChildIndex.js'),
+    output: {
+        path: path.join(__dirname, 'build'),
+        filename: 'assets/js/[name].[contenthash].js'
+    },
     resolve: {
         extensions: [
             '.ts', '.js'
