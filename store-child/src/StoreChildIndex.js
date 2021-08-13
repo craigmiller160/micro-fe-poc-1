302f77b599b1e285de1ec619b015cc685336f231
Craig <craigmiller160@gmail.com>
2021-08-13 09:19:20 -0400

    More setup work

--- store-child/src/StoreChildIndex.js
+++ store-child/src/StoreChildIndex.js
@@ -1,13 +1,1 @@
-import {proxy, snapshot, subscribe} from 'valtio';
-
-const store = proxy({
-    name: ''
-});
-
-export const setName = (name) => {
-    store.name = name;
-};
-
-export const storeSubscribe = (callback) => subscribe(store, () => callback(snapshot(store)));
-
-export const getState = () => snapshot(store);
+import('./StoreBootstrap');
